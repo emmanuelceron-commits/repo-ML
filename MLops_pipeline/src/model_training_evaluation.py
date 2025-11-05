605829255c2cace4765a1a13d3011c62a711c2f5
Emmanuel <emmanuel.ceronxd@outlook.es>
2025-11-05 15:00:06 -0500

    Cambios en modelos

--- MLops_pipeline/src/model_training_evaluation.py
+++ MLops_pipeline/src/model_training_evaluation.py
@@ -53,6 +53,10 @@ if __name__ == "__main__":
         RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42), 
         "RandomForest_MaxDepth10"
     ))
+    results.append(build_model(
+        RandomForestClassifier(n_estimators=200, max_depth=None, random_state=42), 
+        "RandomForest_MaxDepthNone"
+    ))
     results.append(build_model(DecisionTreeClassifier(random_state=42), "DecisionTree"))
     results.append(build_model(GradientBoostingClassifier(n_estimators=200, random_state=42), "GradientBoosting"))
     results.append(build_model(XGBClassifier(n_estimators=200, use_label_encoder=False, eval_metric='logloss', random_state=42,n_jobs=-1), "XGBoost"))
